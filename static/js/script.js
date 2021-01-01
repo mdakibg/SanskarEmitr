5fce447586381cd4481a9feb89c71ba02779cd45
mdakibg <mohdakibgour@gmail.com>
2021-01-01 17:49:57 +0530

    dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -46,3 +46,9 @@ $(document).ready(function() {
     });
 
 });
+
+/* When the user clicks on the button,
+  toggle between hiding and showing the dropdown content */
+  function dropdownFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
